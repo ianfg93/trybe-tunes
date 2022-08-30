0c4245b6fa4969b46dc53f4d962b960575e41d52
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-08-30 15:22:58 -0300

    começando o projeot e requsito 1 feito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <BrowserRouter>
+        <p>TrybeTunes</p>
+        <switch>
+          <div data-testid="page-login">
+            <Route exact path="/" component="Login" />
+          </div>
+          <div data-testid="page-search">
+            <Route exact path="/search" component="Search" />
+          </div>
+          <div data-testid="page-album">
+            <Route exact path="/album/:id" component="Login" />
+          </div>
+          <div data-testid="page-favorites">
+            <Route exact path="/favorites" component="Favorites" />
+          </div>
+          <div data-testid="page-profile">
+            <Route exact path="/profile" component="Profile" />
+          </div>
+          <div data-testid="page-profile-edit">
+            <Route exact path="/profile/edit" component="ProfileEdit" />
+          </div>
+          <div data-testid="page-not-found">
+            <Route path="/rota-nao-mapeada" component="NotFound" />
+          </div>
+        </switch>
+      </BrowserRouter>
+    );
   }
 }
 
