f1547495e0c6c3576c36bee777da25eced115e7a
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-08-31 16:00:17 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    artista: '',
+  };
+
+  handleChange = (event) => {
+    const { target } = event;
+    const { value } = target;
+    this.setState({
+      artista: value,
+    });
+  };
+
   render() {
+    const { artista } = this.state;
+    const minimo = 2;
     return (
       <div>
         <section data-testid="page-search">Search</section>
         <Header />
+        <label htmlFor="search-artist-input">
+          Nome
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ artista }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="search-artist-button"
+          disabled={ artista.length < minimo }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
