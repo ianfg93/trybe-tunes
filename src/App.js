ffd66aea0768661cc984712bd8044c9ceb032878
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-08-31 15:33:21 -0300

    corrigindo req 1

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route path="/rota-nao-mapeada" component={ NotFound } />
+          <Route path="" component={ NotFound } />
         </Switch>
       </div>
     );
